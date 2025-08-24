9e7b0531e58ee2f04c71e1b0408ec59d33fd6243
Jhonatan <naylorelectronics@gmail.com>
2025-08-24 20:04:02 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php require("style.php"); ?>
+
+<!DOCTYPE html5>
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biblioteca Virtual</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body class="theme1">
+    <header class="upper">
+        <div class="titleldesquerdo"><h1>Biblioteca Virtual</h1></div>
+        <div class="nav">
+            <div class="books"><h3>Books</h3></div>
+            <div class="articles"><h3>Articles</h3></div>
+            <div class="about"><h3>About</h3></div>
+            <div class="community"><h3>Community</h3></div>
+            <div class="audiobooks"><h3>Audiobooks</h3></div>
+        </div>
+    </header>
+    <main class="content">
+        <div class="search">
+            <input type="text" id="searchInput" placeholder="pesquisar">
+        </div>
+    </main>
+    <script src="script.js"></script>
+</body>
+</html>
